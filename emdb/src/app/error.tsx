e70ca954e3e787588a3f15b8cd0f64ab76ac1fe6
ephrem <ephremmamo555@gmail.com>
2024-09-27 15:50:04 +0300

    error handling added

---
+++ emdb/src/app/error.tsx
@@ -0,0 +1,17 @@
+'use client';
+export default function error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div className="text-center">
+      <h3>Something went wrong. Please Try Again</h3>
+      <button className="bg-blue-600 py-1 px-2 mt-2 text-white" onClick={reset}>
+        Try Again
+      </button>
+    </div>
+  );
+}
